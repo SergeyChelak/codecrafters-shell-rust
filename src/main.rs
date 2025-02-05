70800f419d7df1dc8f5de47f3646c4518bb56aa1
Sergey Chelak <sergey.chelak@gmail.com>
2025-02-05 02:10:43 +0200

    added echo

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,39 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        check_if_exit(&input);
-        println!("{}: command not found", input.trim());
+        dispatch(&input);
     }
 }
 
-fn check_if_exit(command: &str) {
-    if command.starts_with("exit") {
-        std::process::exit(0)
+fn dispatch(command: &str) {
+    let command = command.trim();
+
+    if try_echo(command) {
+        return;
     }
+
+    if try_exit(command) {
+        return;
+    }
+
+    println!("{}: command not found", command);
+}
+
+const CMD_ECHO: &str = "echo";
+const CMD_EXIT: &str = "exit";
+
+fn try_echo(command: &str) -> bool {
+    if !command.starts_with(CMD_ECHO) {
+        return false;
+    }
+    let output = &command[CMD_ECHO.len() + 1..];
+    println!("{}", output);
+    true
+}
+
+fn try_exit(command: &str) -> bool {
+    if command.starts_with(CMD_EXIT) {
+        std::process::exit(0);
+    }
+    false
 }
