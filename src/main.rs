fc0aebcf680ede8ab85fdef525db568a96b09a01
Sergey Chelak <sergey.chelak@gmail.com>
2025-02-05 22:22:54 +0200

    single quote parse args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod builtins;
 mod os;
 
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    iter,
+};
 
 use builtins::{dispatch_builtin, Builtin};
 use os::{find_file, get_search_path};
@@ -48,10 +51,56 @@ fn exec(program: &str, args: &str) -> bool {
     }
     let mut process = std::process::Command::new(program);
     if !args.is_empty() {
-        process.args(&[args]);
+        let args = parse_args(args);
+        process.args(&args);
     }
     let Ok(mut child) = process.spawn() else {
         return false;
     };
     child.wait().is_ok()
+}
+
+fn parse_args(args: &str) -> Vec<String> {
+    let mut is_enclosing = false;
+    let mut acc: Vec<char> = Vec::new();
+    let mut tokens: Vec<String> = Vec::new();
+    for ch in args.chars().chain(iter::once('\0')) {
+        if ch == '\'' {
+            is_enclosing = !is_enclosing;
+            continue;
+        }
+        if ch == ' ' && !is_enclosing || ch == '\0' {
+            let token = acc.iter().collect::<String>();
+            // TODO: trim?
+            tokens.push(token);
+            acc.clear();
+            continue;
+        }
+        acc.push(ch);
+    }
+    tokens
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_args_test_1() {
+        let args = "'shell hello'";
+
+        let res = parse_args(args);
+        assert!(res.len() == 1);
+        assert_eq!(res[0], "shell hello")
+    }
+
+    #[test]
+    fn parse_args_test_2() {
+        let args = "'/tmp/file name' '/tmp/file name with spaces'";
+
+        let res = parse_args(args);
+        assert!(res.len() == 2);
+        assert_eq!(res[0], "/tmp/file name");
+        assert_eq!(res[1], "/tmp/file name with spaces");
+    }
 }
