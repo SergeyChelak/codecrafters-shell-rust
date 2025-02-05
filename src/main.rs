94bb070880be33ebe14544d65824ec0c0bbb12e9
Sergey Chelak <sergey.chelak@gmail.com>
2025-02-05 03:24:10 +0200

    exec process

--- src/main.rs
+++ src/main.rs
@@ -41,21 +41,22 @@ fn make_command_registry() -> CommandRegistry {
 }
 
 fn dispatch(input: &str, registry: &CommandRegistry) {
-    let Some((command, args)) = input.trim().split_once(' ') else {
-        invalid_input(input);
+    let input = input.trim();
+    let (command, args) = input.split_once(' ').unwrap_or((input, ""));
+    if let Some(func) = registry.get(command) {
+        func(args);
         return;
     };
 
-    let Some(func) = registry.get(command) else {
-        invalid_input(input);
+    if try_exec(command, args) {
         return;
-    };
+    }
 
-    func(args);
+    invalid_input(input);
 }
 
 fn invalid_input(input: &str) {
-    println!("{}: command not found", input.trim());
+    println!("{}: command not found", input);
 }
 
 fn echo(args: &str) {
@@ -91,6 +92,29 @@ fn type_builtin(args: &str) {
     }
 
     println!("{}: not found", args);
+}
+
+fn try_exec(program: &str, args: &str) -> bool {
+    let Ok(path_list) = get_search_path() else {
+        return false;
+    };
+    for path in path_list
+        .iter()
+        .map(|s| std::path::Path::new(s))
+        .map(|p| p.join(program))
+    {
+        if path.exists() {
+            return exec(path.as_os_str().to_str().unwrap_or_default(), args);
+        }
+    }
+    false
+}
+
+fn exec(program: &str, args: &str) -> bool {
+    let Ok(mut child) = std::process::Command::new(program).args(&[args]).spawn() else {
+        return false;
+    };
+    child.wait().is_ok()
 }
 
 fn get_search_path() -> Result<Vec<String>, std::env::VarError> {
